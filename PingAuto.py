74756fa82405e09ee580fbb4efb557bee9bae0ff
yuu29 <38773002+yuu29@users.noreply.github.com>
2019-05-08 15:48:56 +0900

    Update PingAuto.py

--- PingAuto.py
+++ PingAuto.py
@@ -15,3 +15,7 @@ for i,site in enumerate(IP_List):
     IP_List_w = '/root/work/ICMP/' + IP_List + '.txt'
     with open(IP_List_w , mode='a') as IP_List_save:
         os.system("echo # hping2 -V -c 3 -1 --icmptype 8 " + site + " ; hping2 -V -I eth0 -c 3 -1 --icmptype 8 " + site + "; echo # hping2 -V -c 3 -1 --icmptype 13 " + site + "; hping2 -V -I eth0 -c 3 -1 --icmptype 13 " + site + "; echo # hping2 -V -c 3 -1 --icmptype 17 " + site + "; hping2 -V -I eth0 -c 3 -1 --icmptype 17 " + site)
+
+        
+        cmd_str = "script -a -t -c 'echo -e \"\\r\\n# hping2 -V -c 3 -1 --icmptype 8 " + IP + "\"; hping2 -V -I eth0 -c 3 -1 --icmptype 8 " + IP + "; echo -e \"\\r\\n# hping2 -V -c 3 -1 --icmptype 13 " + IP + "\"; hping2 -V -I eth0 -c 3 -1 --icmptype 13 " + IP + "; echo -e \"\\r\\n# hping2 -V -c 3 -1 --icmptype 17 " + IP + "\"; hping2 -V -I eth0 -c 3 -1 --icmptype 17 " + IP + "' /root/work/" + YEARDAY + "/" + IP + "/icmp_" + IP + ".txt 2>/dev/null"
+subprocess.call(cmd_str, shell=True)
